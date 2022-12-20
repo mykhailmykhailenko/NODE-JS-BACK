53633f5a41c692023e275db043843940206bdcdc
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-12-20 10:09:18 +0200

    http client for VSCode

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const http = require('http');
 
 const requestHandler = (request, response)=>{
-    response.end('<html><h1>HELLO YOU</h1></html>');
+    if(request.method === 'GET') {
+        response.end('<h1>YOU SEND A GET REQUEST</h1>');
+    } 
+    else if(request.method === 'POST') {
+        response.end('<h1>YOU SEND A POST REQUEST</h1>'); 
+    } else {
+        response.end('<h1>YOU SEND ANOTHER REQUEST</h1>');
+    }
+
 };
 
 const server = http.createServer(requestHandler);
-
 server.listen(3000, () => {
     console.log('app is started')
 });
